dbb659eb2af4132c98e87bbc840e8c35bd1574a7
MarioYoussef <marioyoussef08@gmail.com>
2025-07-19 18:19:11 +0300

    build home view

--- lib/features/home_feature/presentation/views/home_view.dart
+++ lib/features/home_feature/presentation/views/home_view.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:simple_e_commerce/core/utils/app_fonts/app_fonts.dart';
+import 'package:simple_e_commerce/features/cart_feature/presentation/views/cart_view.dart';
+import 'package:simple_e_commerce/features/product_feature/presentation/views/product_view.dart';
+import 'package:simple_e_commerce/features/profile_feature/presentation/views/profile_view.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+  static const List<Widget> screens = [
+    ProductView(),
+    CartView(),
+    ProfileView(),
+  ];
+  static const List<String> titles = ['Products', 'Cart', 'Profile'];
+  static int selectedIndex = 0;
 
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home View'), centerTitle: true),
-      body: const Column(),
+      appBar: AppBar(
+        title: Text(
+          HomeView.titles[HomeView.selectedIndex],
+          style: AppFonts.textStyleBold24,
+        ),
+        centerTitle: true,
+      ),
+      body: HomeView.screens[HomeView.selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            HomeView.selectedIndex = index;
+          });
+        },
+        currentIndex: HomeView.selectedIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Products'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
     );
   }
 }
