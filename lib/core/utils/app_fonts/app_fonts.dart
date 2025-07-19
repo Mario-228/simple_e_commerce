35e149a1622b75889f78e95b5dce851d74c90d95
MarioYoussef <marioyoussef08@gmail.com>
2025-07-19 15:30:18 +0300

    create app fonts class

---
+++ lib/core/utils/app_fonts/app_fonts.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+import 'package:simple_e_commerce/core/utils/app_colors/app_colors.dart';
+
+abstract class AppFonts {
+  static const TextStyle textStyleMedium6 = TextStyle(
+    color: AppColors.glodenOrange,
+    fontSize: 6,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyleRegular8 = TextStyle(
+    color: AppColors.black,
+    fontSize: 8,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleMedium8 = TextStyle(
+    color: AppColors.black,
+    fontSize: 8,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyleMedium10 = TextStyle(
+    color: AppColors.darkGreen,
+    fontSize: 10,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyleLight10 = TextStyle(
+    color: AppColors.black,
+    fontSize: 10,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w300,
+  );
+  static const TextStyle textStyleRegular12 = TextStyle(
+    color: AppColors.grey,
+    fontSize: 12,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleRegularDarkGrey14 = TextStyle(
+    color: AppColors.darkGrey,
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleMedium12 = TextStyle(
+    color: AppColors.black,
+    fontSize: 12,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyleLight13 = TextStyle(
+    color: AppColors.black,
+    fontSize: 13,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w300,
+  );
+  static const TextStyle textStyleMeduim13 = TextStyle(
+    color: AppColors.darkGrey,
+    fontSize: 13,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyleMeduimWhite13 = TextStyle(
+    color: AppColors.white,
+    fontSize: 13,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyleBold13 = TextStyle(
+    color: AppColors.orange,
+    fontSize: 13,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyleBoldOrange16 = TextStyle(
+    color: AppColors.orange,
+    fontSize: 16,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyleSemiBold14 = TextStyle(
+    color: AppColors.blackShade800,
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle textStyleSemiBold8 = TextStyle(
+    color: AppColors.greyShade800,
+    fontSize: 8,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle textStyleRegular14 = TextStyle(
+    color: AppColors.black,
+    fontSize: 14,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w400,
+    height: 0.08,
+    letterSpacing: -0.17,
+  );
+  static const TextStyle textStyleRegular15 = TextStyle(
+    color: AppColors.greyShade700,
+    fontSize: 14,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleRegularGrey14 = TextStyle(
+    color: AppColors.grey,
+    fontSize: 14,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w400,
+    height: 0.08,
+    letterSpacing: -0.17,
+  );
+  static const TextStyle textStyleRegular20 = TextStyle(
+    color: AppColors.blackShade800,
+    fontSize: 20,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.normal,
+  );
+  // static const TextStyle textStyleSemiBold15 = TextStyle(
+  //   color: AppColors.black,
+  //   fontSize: 15,
+  //   fontFamily: 'SF Pro Text',
+  //   fontWeight: FontWeight.w600,
+  //   height: 0.08,
+  //   letterSpacing: -0.17,
+  // );
+  static const TextStyle textStyleSemiBold15 = TextStyle(
+    color: AppColors.darkGreen,
+    fontSize: 15,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle textStyleBold16 = TextStyle(
+    color: AppColors.beige,
+    fontSize: 16,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyleDarkGreenBold16 = TextStyle(
+    color: AppColors.darkGreen,
+    fontSize: 16,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyleRegular16 = TextStyle(
+    color: AppColors.black,
+    fontSize: 16,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleMostBold18 = TextStyle(
+    color: AppColors.lightLimeGreen,
+    fontSize: 18,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w900,
+  );
+  static const TextStyle textStyleBold20 = TextStyle(
+    color: AppColors.darkGrey,
+    fontSize: 20,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyleBold22 = TextStyle(
+    color: AppColors.darkGreen,
+    fontSize: 22,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyleBold19 = TextStyle(
+    color: AppColors.white,
+    fontSize: 19,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyleSemiBold20 = TextStyle(
+    color: AppColors.blackShade800,
+    fontSize: 20,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle textStyleBold28 = TextStyle(
+    color: AppColors.darkGreen,
+    fontSize: 28,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyleBold24 = TextStyle(
+    color: AppColors.orange,
+    fontSize: 24,
+    fontFamily: 'Lufga',
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle textStyleGreySemiBold20 = TextStyle(
+    color: AppColors.greyShade800,
+    fontSize: 20,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+  );
+}
